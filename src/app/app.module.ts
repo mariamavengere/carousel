f08f1321225ab654c21a87220af70006fec8be44
❌ An error occurred when trying to deploy <mary.mavengere@nacelleaviation.com>
2021-09-11 10:31:17 +0200

    my project

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CardCarouselComponent } from './card-carousel/card-carousel.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CardCarouselComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
